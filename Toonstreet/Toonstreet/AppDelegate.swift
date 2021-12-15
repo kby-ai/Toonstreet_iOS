532a2021900cc4638f1aa7e993a7fa4061c7a4f0
Sholom Scheiner <sholomsheiner@gmail.com>
2021-12-15 17:26:47 -0500

    Test change

--- Toonstreet/Toonstreet/AppDelegate.swift
+++ Toonstreet/Toonstreet/AppDelegate.swift
@@ -13,8 +13,7 @@ import IQKeyboardManagerSwift
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
