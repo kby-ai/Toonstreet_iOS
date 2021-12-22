4276f5b1fe9c10773a153fedd21ceb64e90716e4
kavinsoni01 <kavin.soni01@gmail.com>
2021-12-22 23:29:43 +0530

    PDF view changes

--- Toonstreet/Toonstreet/ViewController/PDFViewController/PDFViewController.swift
+++ Toonstreet/Toonstreet/ViewController/PDFViewController/PDFViewController.swift
@@ -120,6 +120,7 @@ class PDFViewController: BaseViewController {
 //                        pdfView.goToNextPage(nil)
 //                    }
                     
+                    
                     selectedIndex = selectedIndex + 1
 
                         if selectedIndex < self.episodeList?.count ?? 0{
@@ -139,6 +140,7 @@ class PDFViewController: BaseViewController {
                              
                                   if (url != nil) {
                                       self.imgComic.sd_setImage(with: url, completed: nil)
+                                      self.imgComic.slideUpFromBottom()
                                   }
                               }
                             }
@@ -168,6 +170,8 @@ class PDFViewController: BaseViewController {
                          
                               if (url != nil) {
                                   self.imgComic.sd_setImage(with: url, completed: nil)
+                                  self.imgComic.slideUpFromTop()
+
                               }
                           }
                         }
@@ -314,4 +318,51 @@ func tabsVisiblty(_ isVisiblty: Bool = true){
         self.layer.zPosition = -1
     }
 }
+}
+
+
+extension UIView {
+   // Name this function in a way that makes sense to you...
+    // slideFromLeft, slideRight, slideLeftToRight, etc. are great alternative names
+    
+    func slideUpFromTop(duration: TimeInterval = 1.0, completionDelegate: AnyObject? = nil) {
+        // Create a CATransition animation
+        let slideInFromLeftTransition = CATransition()
+
+        // Set its callback delegate to the completionDelegate that was provided (if any)
+        if let delegate: AnyObject = completionDelegate {
+            slideInFromLeftTransition.delegate = delegate as! CAAnimationDelegate
+        }
+
+        // Customize the animation's properties
+        slideInFromLeftTransition.type = CATransitionType.push
+        slideInFromLeftTransition.subtype = CATransitionSubtype.fromBottom
+        slideInFromLeftTransition.duration = duration
+        slideInFromLeftTransition.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        slideInFromLeftTransition.fillMode = CAMediaTimingFillMode.removed
+
+        // Add the animation to the View's layer
+        self.layer.add(slideInFromLeftTransition, forKey: "slideInFromLeftTransition")
+    }
+    
+    
+    func slideUpFromBottom(duration: TimeInterval = 1.0, completionDelegate: AnyObject? = nil) {
+        // Create a CATransition animation
+        let slideInFromLeftTransition = CATransition()
+
+        // Set its callback delegate to the completionDelegate that was provided (if any)
+        if let delegate: AnyObject = completionDelegate {
+            slideInFromLeftTransition.delegate = delegate as! CAAnimationDelegate
+        }
+
+        // Customize the animation's properties
+        slideInFromLeftTransition.type = CATransitionType.push
+        slideInFromLeftTransition.subtype = CATransitionSubtype.fromTop
+        slideInFromLeftTransition.duration = duration
+        slideInFromLeftTransition.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        slideInFromLeftTransition.fillMode = CAMediaTimingFillMode.removed
+
+        // Add the animation to the View's layer
+        self.layer.add(slideInFromLeftTransition, forKey: "slideInFromLeftTransition")
+    }
 }
